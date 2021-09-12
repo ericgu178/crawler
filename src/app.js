1b2828b17e92d0cb0d438990cd6d0d26b1cdfb3b
EricGU178 <1143055543@qq.com>
2021-09-12 10:59:23 +0800

    app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from 'react-redux';
 import { BrowserRouter , Switch, Route } from 'react-router-dom';
 import index from "./views/main";
-// app.js linux 莫名其妙的 大写了？？
+
 class App extends React.Component {
     render() {
         const { store } = this.props
